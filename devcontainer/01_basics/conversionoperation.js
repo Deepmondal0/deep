9d4948942d924c2bd4447cbf9ad2938f28ebb4b7
Deepmondal0 <dmsaratnagar@gmail.com>
2024-11-30 17:34:55 +0000

    convertion of datatype

---
+++ devcontainer/01_basics/conversionoperation.js
@@ -0,0 +1,29 @@
+//let score = "30abc"
+
+//const {score} = req.body
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//NaN = not a number
+
+// "30" => 30
+// "30abc" => NaN
+
+let isLoggedIn = ""
+
+let booleanIsLogedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLogedIn);
+
+
+//1 => true , 0 => false
+//"" => false
+//"deep" => true 
+
+let someNumber = 333
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
