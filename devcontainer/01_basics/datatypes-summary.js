2265ed2feb664ff4199570a5a2fbd2f8b330a5c2
Deepmondal0 <dmsaratnagar@gmail.com>
2024-12-02 06:17:08 +0000

    datatypes-summary

---
+++ devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedin = false
+// const outsideTem = null
+// let userEmail; = undefined
+
+const id = symbol("111")
+const anotherId = Symbol("111")
+
+console.log(id === anotherId);
+
+
+// Reference (Non primitive )
+
+//Array, Object, Functions
