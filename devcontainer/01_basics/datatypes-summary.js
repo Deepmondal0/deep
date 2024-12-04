23ea9662fb2a5c98a1bab4369e513282ed567aae
Deepmondal0 <dmsaratnagar@gmail.com>
2024-12-04 17:29:41 +0000

    strings.js

--- devcontainer/01_basics/datatypes-summary.js
+++ devcontainer/01_basics/datatypes-summary.js
@@ -10,12 +10,37 @@
 // const outsideTem = null
 // let userEmail; = undefined
 
-const id = symbol("111")
-const anotherId = Symbol("111")
+// const id = symbol("111")
+// const anotherId = Symbol("111")
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 
 // Reference (Non primitive )
 
 //Array, Object, Functions
+
+//***********************************************************************
+
+// memory two types : Stack(primitive) , Heap(non primitive)
+
+let myName = "DeepMondal"
+
+let anotherName = myName
+anotherName = "sagar"
+console.log(anotherName);
+console.log(myName);
+
+let userone ={
+    email: "dmsaratnagar@gmail.com",
+    upi: "user@ybl"
+
+}
+
+usertwo = userone
+
+usertwo.email = "deep@google.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
+
