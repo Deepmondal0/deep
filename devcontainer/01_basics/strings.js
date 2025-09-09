cfcc921b83897c44755e50be00ab34c1264bff2c
Deepmondal0 <dmsaratnagar@gmail.com>
2025-09-09 23:12:28 +0530

    Delete devcontainer directory

--- devcontainer/01_basics/strings.js
+++
@@ -1,8 +0,0 @@
-const name = "DeepMondal"
-const repocount = 50
-
-// console.log(name + repocount +"Value");
-
-console.log(`Hello my name is ${name}and my repo count is ${repocount}`);
-
-const gameName = new String('Deep')
